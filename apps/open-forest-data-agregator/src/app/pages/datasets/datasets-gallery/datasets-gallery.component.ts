865261506a823a384eb2d3492205719044ee9cea
Maciej Syguła <mac.sygula@gmail.com>
2020-05-21 20:13:46 +0200

    Add datasets gallery view with slider

--- apps/open-forest-data-agregator/src/app/pages/datasets/datasets-gallery/datasets-gallery.component.ts
+++ apps/open-forest-data-agregator/src/app/pages/datasets/datasets-gallery/datasets-gallery.component.ts
@@ -1,14 +1,49 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+
+import 'lightgallery.js';
+import 'lg-zoom.js';
+import 'lg-autoplay.js';
+import 'lg-fullscreen.js';
+import 'lg-thumbnail.js';
+
+import { IUISelectOptions } from '@libs/ui-select/src/lib/ui-select/ui-select.component';
 
 @Component({
   selector: 'ofd-agregator-datasets-gallery',
   templateUrl: './datasets-gallery.component.html',
   styleUrls: ['./datasets-gallery.component.scss']
 })
-export class DatasetsGalleryComponent implements OnInit {
+export class DatasetsGalleryComponent implements OnInit, AfterViewInit {
+  /*
+   * MOCK
+   */
+  public page = 1;
+
+  public sortItems = [
+    { name: 'A-Z', value: 0 },
+    { name: 'Z-A', value: 0 }
+  ];
+
+  public sortBy = null;
+
+  public options: IUISelectOptions = {
+    placeholder: 'Sortuj wg'
+  };
+
   @Input() datasets: any[];
+
+  @ViewChild('galleryItems', { static: false }) galleryItems: any;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      // @ts-ignore
+      window.lightGallery(document.querySelector('.datasets-list__datasets'), {
+        selector: '.datasets-list__datasets__item__picture'
+      });
+    }, 0);
+  }
 }
