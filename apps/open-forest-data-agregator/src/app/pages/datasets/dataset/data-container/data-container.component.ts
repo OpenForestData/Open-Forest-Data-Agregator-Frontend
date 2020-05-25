50fc3861f0e16ed0a93d15dcd8dc6f6bdd2c29d2
Mateusz Gorczycki <mateusz.gorczycki@whiteaster.com>
2020-05-25 14:30:10 +0200

    Merge dataset-metadata-all to development

---
+++ apps/open-forest-data-agregator/src/app/pages/datasets/dataset/data-container/data-container.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'ofd-agregator-data-container',
+  templateUrl: './data-container.component.html',
+  styleUrls: ['./data-container.component.scss']
+})
+export class DataContainerComponent implements OnInit {
+  @Input() iconSrc: string;
+
+  @Input() expandable: boolean;
+
+  @Input() expanded: boolean;
+
+  @Input() title: string;
+
+  @Output() toggle: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onToggle() {
+    this.expanded = !this.expanded;
+    this.toggle.emit();
+  }
+}
