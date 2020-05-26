d57a8977c448633c2b4b039ad2f8913a5f527733
Daniel Pasoń <daniel.pason@whiteaster.com>
2020-05-26 13:49:52 +0200

    Blog page, subpage and blog mobile for both

---
+++ apps/open-forest-data-agregator/src/app/pages/blog/blog.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '@app/shared/shared.module';
+
+import { BlogComponent } from './blog.component';
+import { BlogRoutingModule } from './blog-routing.module';
+import { PostTileComponent } from './post-tile/post-tile.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+
+const components = [BlogComponent, PostTileComponent, BlogPostComponent];
+
+@NgModule({
+  declarations: components,
+  imports: [CommonModule, BlogRoutingModule, SharedModule]
+})
+export class BlogModule {}
