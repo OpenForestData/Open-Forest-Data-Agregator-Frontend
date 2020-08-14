cab719ef0c81bc66a28fc9f2b475373e80f44a52
Maciej Syguła <mac.sygula@gmail.com>
2020-08-14 13:50:03 +0200

    Redicrect to home page when language was changed

--- apps/open-forest-data-agregator/src/app/layout/header/header-actions/header-actions.component.ts
+++ apps/open-forest-data-agregator/src/app/layout/header/header-actions/header-actions.component.ts
@@ -64,7 +64,7 @@ export class HeaderActionsComponent implements OnInit, OnDestroy {
    */
   setNewLanguage(value: string) {
     if (this.languageService.language !== value) this.languageService.language = value;
-    window.location.reload();
+    window.location.href = '/';
   }
 
   /**
