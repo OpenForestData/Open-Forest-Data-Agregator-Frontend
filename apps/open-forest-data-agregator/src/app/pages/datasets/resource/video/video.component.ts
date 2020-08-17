4ac77b39c6c994acba976fe74151a7f3c362c23f
Mateusz Gorczycki <mateusz.gorczycki@whiteaster.com>
2020-08-17 15:02:26 +0200

    Add documentation

--- apps/open-forest-data-agregator/src/app/pages/datasets/resource/video/video.component.ts
+++ apps/open-forest-data-agregator/src/app/pages/datasets/resource/video/video.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+/**
+ * Video component
+ */
 @Component({
   selector: 'ofd-agregator-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss']
 })
-export class VideoComponent implements OnInit {
+export class VideoComponent {
+  /**
+   * Resource
+   */
   @Input() resource;
-
-  constructor() {}
-
-  ngOnInit() {}
 }
