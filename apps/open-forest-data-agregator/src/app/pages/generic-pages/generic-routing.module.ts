1687682d21267a5920e2b3e639e06debf1b5e585
Maciej Syguła <mac.sygula@gmail.com>
2021-03-04 15:14:30 +0100

    Remove instructions route

--- apps/open-forest-data-agregator/src/app/pages/generic-pages/generic-routing.module.ts
+++ apps/open-forest-data-agregator/src/app/pages/generic-pages/generic-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FaqComponent } from './faq/faq.component';
-import { InstructionsComponent } from './instructions/instructions.component';
 import { PartnersComponent } from './partners/partners.component';
 import { PageTemplateComponent } from '@app/shared/page-template/page-template.component';
 
@@ -9,7 +8,7 @@ const routes: Routes = [
   // { path: 'about-project', component: AboutProjectComponent },
   // { path: 'about-resources', component: AboutResourcesComponent },
   { path: 'faq', component: FaqComponent },
-  { path: 'instructions', component: InstructionsComponent },
+  // { path: 'instructions', component: InstructionsComponent },
   { path: 'partners', component: PartnersComponent },
   { path: ':slug', component: PageTemplateComponent }
 ];
