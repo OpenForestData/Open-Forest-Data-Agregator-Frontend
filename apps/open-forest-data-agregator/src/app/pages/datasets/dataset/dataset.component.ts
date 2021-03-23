eeac0964cd254c5c77df440b047c1f1157a7194d
Maciej Syguła <mac.sygula@gmail.com>
2021-03-23 13:23:37 +0100

    Remove query params from meteo

--- apps/open-forest-data-agregator/src/app/pages/datasets/dataset/dataset.component.ts
+++ apps/open-forest-data-agregator/src/app/pages/datasets/dataset/dataset.component.ts
@@ -238,10 +238,11 @@ export class DatasetComponent implements OnInit {
    */
   makeSource() {
     if (this.dataset?.alternativeURL) {
-      const source = this.dataset?.search_info?.results[0]?.dataSources
-        ? this.dataset?.search_info?.results[0]?.dataSources[0]
-        : null;
-      return this.dataset?.alternativeURL + (source === 'Grafana' ? '?from=now-7d&to=now' : '');
+      // const source = this.dataset?.search_info?.results[0]?.dataSources
+      //   ? this.dataset?.search_info?.results[0]?.dataSources[0]
+      //   : null;
+      // return this.dataset?.alternativeURL + (source === 'Grafana' ? '?from=now-7d&to=now' : '');
+      return this.dataset?.alternativeURL;
     } else {
       return `${AppConfigService.config.dataverseURL}/dataset.xhtml?persistentId=${this.dataset.latestVersion?.datasetPersistentId}`;
     }
